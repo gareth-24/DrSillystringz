9c96c2940a17c35bf53bc3776f2a2430a43b9e1d
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-26 10:08:34 -0700

    add home controller and initial splash page view file

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -5,8 +5,25 @@ using System.Linq;
 
 namespace Factory.Controllers
 {
-  public class HomeContoller : Controller
+  public class HomeController : Controller
   {
-    
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Engineer[] engineers = _db.Engineers.ToArray();
+      Machine[] machines = _db.Machines.ToArray();
+      Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+      model.Add("engineers", engineers);
+      model.Add("machines", machines);
+      return View(model);
+    }
+
   }
 }
