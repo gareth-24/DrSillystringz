b6fdac0069e5928cacf6145f2fd10a84870abc4e
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-26 09:13:15 -0700

    add initial empty controller files

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class HomeContoller : Controller
+  {
+    
+  }
+}
